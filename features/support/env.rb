c84a51dc3620e6a2a194da2183307fcad0ce675e
Rich Downie <rich@blackboxd.com>
2018-12-28 15:18:29 -0500

    adding cukehub

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,7 @@
+require 'cukehub'
+
 Before do
+  @cukehub_api_key = "KMYVate1VU7R2z1QRCbJoq5M"
   Capybara.default_driver = :selenium
   Capybara.app_host = 'https://fullstackautomationwithruby.com'
 end
