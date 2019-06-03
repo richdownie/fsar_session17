52f8a8fe9c430137fbd5c39cb9ab1f39a9ab0238
Rich Downie <rich@blackboxd.com>
2019-06-03 10:24:41 -0400

    adding headless chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,20 @@
 # require 'cukehub'
 
-Before do
+Before do 
   @cukehub_api_key = "KMYVate1VU7R2z1QRCbJoq5M"
-  Capybara.default_driver = :selenium
   Capybara.app_host = 'https://fullstackautomationwithruby.com'
+  
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+     chromeOptions: { args: %w[headless disable-gpu] }
+   )
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+  end
+
+  Capybara.javascript_driver = :chrome
+
+  Capybara.configure do |config|
+    config.default_max_wait_time = 10 # seconds
+    config.default_driver        = :selenium
+  end
 end
