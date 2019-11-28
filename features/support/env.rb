faf78a4d4ff8b4f7a3f9ac1177ca226b79659493
Rich Downie <rich@blackboxd.com>
2019-11-28 11:55:47 -0500

    fixing env hooks files

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,20 +1,7 @@
-# require 'cukehub'
-
-Before do 
-  @cukehub_api_key = "KMYVate1VU7R2z1QRCbJoq5M"
-  Capybara.app_host = 'https://fullstackautomationwithruby.com'
-  
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-     chromeOptions: { args: %w[headless disable-gpu] }
-   )
-  Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-  end
-
-  Capybara.javascript_driver = :chrome
-
-  Capybara.configure do |config|
-    config.default_max_wait_time = 10 # seconds
-    config.default_driver        = :selenium
-  end
-end
+require 'selenium-webdriver'
+require 'webdrivers'
+require 'cucumber'
+require 'rspec'
+require 'pry'
+require 'capybara/cucumber'
+require 'launchy'
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,8 +1,16 @@
-require 'selenium-webdriver'
-require 'webdrivers'
-require 'cucumber'
-require 'rspec'
-require 'pry'
-require 'httparty'
-require 'capybara/cucumber'
-require 'launchy'
+Before do 
+  Capybara.app_host = 'https://fullstackautomationwithruby.com'
+  
+  options = Selenium::WebDriver::Chrome::Options.new(args: ['--headless'])
+  
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  Capybara.javascript_driver = :chrome
+
+  Capybara.configure do |config|
+    config.default_max_wait_time = 10 # seconds
+    config.default_driver        = :selenium
+  end
+end
